6211cb1f0df3256f69769ef47a93c20bbe0fc9fc
Tom Prats <tprats108@gmail.com>
2017-02-03 21:10:10 -0500

    Added Dimension Widgets

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,22 @@
 import { h, Component } from "preact";
+import Dimensions from "./results/dimensions";
 
 export default class App extends Component {
   render() {
+    var language = "en"
+    var translations = {
+      en: {
+        potential_benefits: "Potential Benefits",
+        potential_pitfalls: "Potential Pitfalls",
+        show_less: "Show Less",
+        show_more: "Show More"
+      }
+    }
+    var translate = (key) => translations[language][key]
     return (
-      <div id="app" />
+      <div id="app">
+        <Dimensions translate={translate} />
+      </div>
     );
   }
 }

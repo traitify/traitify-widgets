da78f9d3cb2c41149d036ea4438c70a793c3ecc7
Katie Harron <katie@pibby.com>
2017-02-16 14:32:23 -0600

    Bar Chart styling for mobile to desktop

--- src/components/results/type-based-results/personality-type-bar-chart/index.js
+++ src/components/results/type-based-results/personality-type-bar-chart/index.js
@@ -7,13 +7,11 @@ export default class PersonalityTypeBarChart extends Component {
   render() {
     var types = this.props.assessment.personality_types;
     return (
-      <div class={style.chart}>
-        <div>
-          {types.map(function(type, index) {
-            return <PersonalityTypeBar type={type} index={index} length={types.length} />;
-          })}
-        </div>
-      </div>
+			<ul class={style.chart}>
+				{types.map(function(type, index) {
+					return <PersonalityTypeBar type={type} index={index} length={types.length} />;
+				})}
+			</ul>
     );
   }
 }
--- src/components/results/type-based-results/personality-type-bar/index.js
+++ src/components/results/type-based-results/personality-type-bar/index.js
@@ -6,14 +6,15 @@ import PersonalityTypes from "../personality-types";
 export default class PersonalityTypeBar extends Component {
   render() {
     var type = this.props.type.personality_type;
+		var typeTitle = `${type.name}`;
+		var typeIcon = `${type.badge.image_medium}`;
     var color = `#${type.badge.color_1}`;
     var score = Math.round(this.props.type.score);
-    var width = 1 / this.props.length * 100;
-    var left = 100 / this.props.length * this.props.index;
     return (
-      <div class={style.bar} style={`background: ${color}; height: ${score}%; width: ${width}%; left: ${left}%;`}>
-        <span class={style.text}>{score}%</span>
-      </div>
+      <li class={style.bar} data-bg={`${color}`}>
+        <span class={style.barScore} style={`background: ${color}; height: ${score}%;`}>{score}%</span>
+				<span class={style.barLabel}><img src={`${typeIcon}`} alt={`${typeTitle}`} /><i>{typeTitle}</i></span>
+      </li>
     );
   }
 }

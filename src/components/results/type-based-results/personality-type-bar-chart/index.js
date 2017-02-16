20d5da304604b480a706bf9feda88aeb19859201
Carson Wright <carsonnwright@gmail.com>
2017-02-16 17:04:44 -0500

    added select and hover

--- src/components/results/type-based-results/personality-type-bar-chart/index.js
+++ src/components/results/type-based-results/personality-type-bar-chart/index.js
@@ -4,12 +4,24 @@ import style from "./style";
 import PersonalityTypeBar from "../personality-type-bar";
 
 export default class PersonalityTypeBarChart extends Component {
+  handleMouseLeave(){
+
+    let props = this.props.assessment.personality_types.map((type)=>{
+      type.personality_type.hover = false
+
+      return type
+    })
+
+    this.props.setState(props);
+	}
+
   render() {
+    let com = this;
     var types = this.props.assessment.personality_types;
     return (
-			<ul class={style.chart}>
+			<ul class={style.chart} onMouseLeave={this.handleMouseLeave.bind(this)}>
 				{types.map(function(type, index) {
-					return <PersonalityTypeBar type={type} index={index} length={types.length} />;
+					return <PersonalityTypeBar {...com.props} type={type} index={index} length={types.length} />;
 				})}
 			</ul>
     );
--- src/components/results/type-based-results/personality-type-bar/index.js
+++ src/components/results/type-based-results/personality-type-bar/index.js
@@ -4,17 +4,39 @@ import style from "./style";
 import PersonalityTypes from "../personality-types";
 
 export default class PersonalityTypeBar extends Component {
-  render() {
-    var type = this.props.type.personality_type;
+	constructor(){
+		super()
+		this.state = {}
+		return this
+	}
+
+  handleColor(key, value){
+		let index = this.props.index
+    let props = this.props.assessment.personality_types.map((type, i)=>{
+      type.personality_type[key] = i == index ? true : false;
+
+      return type
+    })
+
+    this.props.setState(props);
+	}
+
+	render() {
+		var type = this.props.type.personality_type;
 		var typeTitle = `${type.name}`;
 		var typeIcon = `${type.badge.image_medium}`;
-    var color = `#${type.badge.color_1}`;
-    var score = Math.round(this.props.type.score);
-    return (
-      <li class={style.bar} data-bg={`${color}`}>
+		var color = `#${type.badge.color_1}`;
+		var score = Math.round(this.props.type.score);
+		var spanStyle = ''
+
+		if(type.hover || type.clicked){
+			spanStyle = `background-color: ${color}`;
+		}
+		return (
+			<li class={`${style.bar} ${type.clicked == true ? style.selected :'' }`} onMouseOver={this.handleColor.bind(this, 'hover', true)} onMouseOver={this.handleColor.bind(this, 'hover', false)} onClick={this.handleColor.bind(this, 'clicked', true)}>
         <span class={style.barScore} style={`background: ${color}; height: ${score}%;`}>{score}%</span>
-				<span class={style.barLabel}><img src={`${typeIcon}`} alt={`${typeTitle}`} /><i>{typeTitle}</i></span>
-      </li>
-    );
-  }
+        <span class={style.barLabel} style={spanStyle}><img src={`${typeIcon}`} alt={`${typeTitle}`} /><i>{typeTitle}</i></span>
+			</li>
+		);
+	}
 }

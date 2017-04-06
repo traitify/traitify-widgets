f01ff3de79691744e8e39dbc8a838acc52cf7bbe
Carson Wright <carsonnwright@gmail.com>
2017-04-05 22:52:04 -0400

    add fingerprint support (#47)

--- src/lib/traitify.js
+++ src/lib/traitify.js
@@ -1,5 +1,6 @@
 import Traitify from "traitify-client";
 Traitify.ui = require("traitify-ui").default;
 Traitify.ui.client = Traitify;
+Traitify.__version__ = __VERSION__;
 require("polyfills");
 export default Traitify;

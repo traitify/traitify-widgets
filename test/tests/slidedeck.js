790a6ad9aa0c08e3e1bf65d2e2ed6bde5e2be75d
Carson Wright <carsonnwright@gmail.com>
2017-04-17 20:45:46 -0400

    fixed slidedeck casing ;)

--- test/tests/slidedeck.js
+++ test/tests/slidedeck.js
@@ -1,50 +1,50 @@
 /*global sinon,expect,StepTest*/
 /*eslint-disable prefer-arrow-callback*/
 function Init(){
-  this.test("Slidedeck Should Initialize")
-          .tag("slidedeck")
-          .tag("event")
-          .tag("initialize")
-          .step("Setup Scratch")
-          .step("Load Traitify UI with SlideDeck assessment id")
-          .step("Set scratch as Target")
-          .step("Render and Wait for SlideDeck to Initialize")
-          .expect("SlideDeck should be Ready", function(){
-            this.ok(this.scratch.innerHTML.indexOf("Not Me") != -1);
-          });
+  this.test("SlideDeck Should Initialize")
+      .tag("SlideDeck")
+      .tag("event")
+      .tag("initialize")
+      .step("Setup Scratch")
+      .step("Load Traitify UI with SlideDeck assessment id")
+      .step("Set scratch as Target")
+      .step("Render and Wait for SlideDeck to Initialize")
+      .expect("SlideDeck should be Ready", function(){
+        this.ok(this.scratch.innerHTML.indexOf("Not Me") != -1);
+      });
 
   this.test("Click Me Event")
-          .tag("slidedeck")
-          .tag("event")
-          .tag("me")
-          .step("Initialize SlideDeck")
-          .step("Listen for", "slidedeck.me")
-          .step("Click", ".traitify--slide-deck--me")
-          .expect("Slide Deck to have Clicked Me", function(){
-            this.ok(this["slidedeck.me"] == true);
-          });
+      .tag("SlideDeck")
+      .tag("event")
+      .tag("me")
+      .step("Initialize SlideDeck")
+      .step("Listen for", "slidedeck.me")
+      .step("Click", ".traitify--slide-deck--me")
+      .expect("Slide Deck to have Clicked Me", function(){
+        this.ok(this["slidedeck.me"] == true);
+      });
     
   this.test("Click Not Me Event")
-          .tag("slidedeck")
-          .tag("event")
-          .tag("not me")
-          .step("Initialize SlideDeck")
-          .step("Listen for", "slidedeck.notMe")
-          .step("Click", ".traitify--slide-deck--notMe")
-          .expect("Slide Deck to have Clicked Not Me", function(){
-            this.ok(this["slidedeck.notMe"] == true);
-          });
+      .tag("SlideDeck")
+      .tag("event")
+      .tag("not me")
+      .step("Initialize SlideDeck")
+      .step("Listen for", "slidedeck.notMe")
+      .step("Click", ".traitify--slide-deck--notMe")
+      .expect("Slide Deck to have Clicked Not Me", function(){
+        this.ok(this["slidedeck.notMe"] == true);
+      });
 
   this.test("Answer Slide Event")
-          .tag("slidedeck")
-          .tag("event")
-          .tag("answer slide")
-          .step("Initialize SlideDeck")
-          .step("Listen for", "slidedeck.AnswerSlide")
-          .step("Click", ".traitify--slide-deck--notMe")
-          .expect("Slide Deck to have Clicked Not Me", function(){
-            this.ok(this["slidedeck.AnswerSlide"] == true);
-          });
+      .tag("SlideDeck")
+      .tag("event")
+      .tag("answer slide")
+      .step("Initialize SlideDeck")
+      .step("Listen for", "slidedeck.AnswerSlide")
+      .step("Click", ".traitify--slide-deck--notMe")
+      .expect("Slide Deck to have Clicked Not Me", function(){
+        this.ok(this["slidedeck.AnswerSlide"] == true);
+      });
 }
 export default Init;
 /*eslint-enable prefer-arrow-callback*/

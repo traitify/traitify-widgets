c3a3210075c940c47295bffd18aace8b3313a730
Carson Wright <carsonnwright@gmail.com>
2017-04-24 14:27:10 -0400

    airbrake fixes

--- test/support/prerun.js
+++ test/support/prerun.js
@@ -2,6 +2,8 @@
 import Factories from "../support/factories";
 import steps from "../steps";
 import Mocks from "../support/mocks";
+import ErrorHandler from "../../src/error-handler";
+
 function Init(){
   let ui = this.Traitify.ui;
   let oldIE = this.Traitify.oldIE;
@@ -14,6 +16,7 @@ function Init(){
   this.Traitify.ui.client = this.Traitify;
   Mocks(this.Traitify);
   steps.apply(this)
+
   /* IMPORTANT
   * No matter the size of the suite top time should be under 200ms
   */

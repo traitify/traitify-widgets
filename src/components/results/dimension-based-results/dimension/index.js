8dc57545cc190a060d2f08678df45481af886ca1
Tom Prats <tprats108@gmail.com>
2017-12-01 16:34:00 -0500

    Added Short Descriptions for Dimensional Types

--- src/components/results/dimension-based-results/dimension/index.js
+++ src/components/results/dimension-based-results/dimension/index.js
@@ -17,6 +17,15 @@ export default class Dimension extends Component{
   componentDidMount(){
     this.props.triggerCallback("Dimension", "initialized", this);
   }
+  description(suffix){
+    let type = this.props.personalityType.personality_type;
+    let perspective = (this.props.perspective || "firstPerson").replace("Person", "");
+    let description = type.details.find(detail=>detail.title === `${perspective}_person_${suffix}`);
+    description = description && description.body;
+    if(description){ return description; }
+    description = type.details.find(detail=>detail.title === `${perspective}_person_${suffix}`);
+    return (description && description.body) || type.description;
+  }
   render(){
     let type = this.props.personalityType.personality_type;
     let benefits = [];
@@ -26,14 +35,6 @@ export default class Dimension extends Component{
       if(detail.title === "Pitfalls") pitfalls.push(detail.body);
     });
     let color = `#${type.badge.color_1}`;
-    let perspective = `${(this.props.perspective || "firstPerson").replace("Person", "")}_person_description`;
-    let description = type.details.find(detail=>detail.title === perspective);
-    description = description && description.body;
-    if(!description){
-      perspective = `${perspective === "first_person_description" ? "third" : "first"}_person_description"`;
-      description = type.details.find(detail=>detail.title === perspective);
-      description = (description && description.body) || type.description;
-    }
 
     return (
       <li class={style.dimension}>
@@ -45,7 +46,7 @@ export default class Dimension extends Component{
           </div>
           <div class={style.content}>
             <h4 class={style.title}>{type.name} <span style={`color: ${color}`}>|</span> {this.props.personalityType.score} - {type.level}</h4>
-            <p class={style.description}>{type.description}</p>
+            <p class={style.description}>{this.description("short_description")}</p>
             <p class={style.triggerButton}><a class={style.trigger} style={`background: ${Color.rgba(color, 30)}`} onClick={this.trigger} href="#">{this.props.translate(this.state.showContent ? "show_less" : "show_more")}</a></p>
           </div>
         </div>
@@ -54,7 +55,7 @@ export default class Dimension extends Component{
             <div class={style.content} style={`background: ${Color.rgba(color, 30)}`}>
               <div class={style.extendedDesc}>
                 <h4>{this.props.translate("extended_description")}</h4>
-                <p class={style.description}>{description}</p>
+                <p class={style.description}>{this.description("description")}</p>
               </div>
               <div class={style.detail}>
                 <h5 class={style.benefits}>{this.props.translate("potential_benefits")}</h5>

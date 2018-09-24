1d3f458899e925dadaa8b01bb265b539796f5661
Eric Fleming <efleming@gmail.com>
2018-09-24 15:24:10 -0400

    Add locale data files for swedish and french france

--- src/lib/i18n-data.js
+++ src/lib/i18n-data.js
@@ -3,6 +3,8 @@ import es from "lib/i18n-data/es.json";
 import fr from "lib/i18n-data/fr.json";
 import ht from "lib/i18n-data/ht.json";
 import nn from "lib/i18n-data/nn.json";
+import frfr from "lib/i18n-data/fr_fr.json";
+import svse from "lib/i18n-data/sv_se.json";
 
 export default {
   "en-us": en,
@@ -10,5 +12,7 @@ export default {
   "fr-ca": fr,
   "fr-us": fr,
   "ht-us": ht,
-  "no-no": nn
+  "no-no": nn,
+  "sv-se": svse,
+  "fr-fr": frfr
 };

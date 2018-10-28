bf530f7f4fefba8b3b9d14b908d6664c943fd0ca
Jbur43 <jtburum@gmail.com>
2018-10-28 08:47:51 -0400

    add new locale data

--- src/lib/i18n-data.js
+++ src/lib/i18n-data.js
@@ -2,16 +2,19 @@ import en from "lib/i18n-data/en.json";
 import es from "lib/i18n-data/es.json";
 import fr from "lib/i18n-data/fr.json";
 import ht from "lib/i18n-data/ht.json";
+import nl from "lib/i18n-data/nl.json";
 import nn from "lib/i18n-data/nn.json";
 import frfr from "lib/i18n-data/fr_fr.json";
 import svse from "lib/i18n-data/sv_se.json";
 
 export default {
   "en-us": en,
+  "en-gb": en,
   "es-us": es,
   "fr-ca": fr,
   "fr-us": fr,
   "ht-us": ht,
+  "nl-nl": nl,
   "no-no": nn,
   "sv-se": svse,
   "fr-fr": frfr

aece5b397de2a96e19c2cff9ab1585e0685ee188
Jbur43 <jtburum@gmail.com>
2019-03-08 08:28:36 -0500

    japanese translations

--- src/lib/i18n-data.js
+++ src/lib/i18n-data.js
@@ -1,10 +1,11 @@
 import en from "lib/i18n-data/en.json";
 import es from "lib/i18n-data/es.json";
 import fr from "lib/i18n-data/fr.json";
+import frfr from "lib/i18n-data/fr_fr.json";
 import ht from "lib/i18n-data/ht.json";
+import ja from "lib/i18n-data/ja.json";
 import nl from "lib/i18n-data/nl.json";
 import nn from "lib/i18n-data/nn.json";
-import frfr from "lib/i18n-data/fr_fr.json";
 import svse from "lib/i18n-data/sv_se.json";
 
 export default {
@@ -14,6 +15,7 @@ export default {
   "fr-ca": fr,
   "fr-us": fr,
   "ht-us": ht,
+  "ja": ja,
   "nl-nl": nl,
   "no-no": nn,
   "sv-se": svse,

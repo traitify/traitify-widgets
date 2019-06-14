cd1d217ba882fbfcd3e646f46a560a7625a2b117
Jbur43 <jtburum@gmail.com>
2019-06-14 12:35:06 -0400

    upate naming and catch invalid arguments

--- src/components/guide/index.js
+++ src/components/guide/index.js
@@ -6,35 +6,47 @@ import {ApolloProvider, Query} from "react-apollo";
 import query from "graphql-queries/guide-for-assessment";
 
 const client = new ApolloClient({
-  headers: {authorization: "Basic ZGhtZ3NnMWQ2MmJxcDhpb2tqaTEzZnVobWY6eA==", mode: "no-cors"},
+  headers: {authorization: "Basic ZGhtZ3NnMWQ2MmJxcDhpb2tqaTEzZnVobWY6eA=="},
   uri: "https://api.stag.awse.traitify.com/v1/interview_guides/graphql"
 });
 
-class InterviewGuide extends Component {
+class Guide extends Component {
   static defaultProps = {assessment: null}
   static propTypes = {
     assessment: PropTypes.shape({assessment_type: PropTypes.string})
   }
-  buildTypes(types) {
-    return types.map((type) => {
+  buildPersonalityTypes(types) {
+    const invalidArguments = [];
+    const toGraphql = types.map((type) => {
       let personalityType = type.personality_type;
+      if(personalityType.level == null) { return invalidArguments.push(null); }
+
       personalityType = {level: personalityType.level, id: personalityType.id};
       return {personalityType};
     });
+
+    if(invalidArguments.length > 0) {
+      // Airbrake?
+      return false;
+    } else {
+      return toGraphql;
+    }
   }
   render() {
     if(!this.props.assessment) { return null; }
-
     const props = this.props.assessment;
+    const personalityTypes = this.buildPersonalityTypes(props.personality_types);
+    if(!personalityTypes) { return <div>Invalid</div>; }
+
     const assessment = {
       id: props.id,
       deckId: props.deck_id,
-      personalityTypes: this.buildTypes(props.personality_types)
+      personalityTypes
     };
 
     return (
       <ApolloProvider client={client}>
-        <div className="interview-guide-container">
+        <div className="guide-container">
           <Query query={query} variables={{assessment}}>
             {({loading, error, data}) => {
               if(loading) return <p>Loading...</p>;
@@ -50,15 +62,5 @@ class InterviewGuide extends Component {
   }
 }
 
-export {InterviewGuide as Component};
-export default withTraitify(InterviewGuide);
-
-// <Query query={query} variables={{assessment}}>
-//   {({loading, error, data}) => {
-//     if(loading) return <p>Loading...</p>;
-//     if(error) return <p>Error :(</p>;
-//
-//     console.log(data, "query data");
-//     return <div>testing please wait a moment</div>;
-//   }}
-// </Query>
+export {Guide as Component};
+export default withTraitify(Guide);
--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,7 @@ export Results from "./results";
 export TypeBasedResults from "./results/type-based-results";
 export DimensionBasedResults from "./results/dimension-based-results";
 export SlideDeck from "./slide-deck";
-export InterviewGuide from "./interview-guide";
+export Guide from "./guide";
 
 export * as DimensionComponents from "./results/dimension-based-results/components";
 export * as TypeComponents from "./results/type-based-results/components";

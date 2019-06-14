ec8be20c7477c9cbe5503a78f2fd0a4b00e80f85
Jbur43 <jtburum@gmail.com>
2019-06-14 11:06:36 -0400

    guideForAssessment added. modified payload to account

--- src/components/interview-guide/index.js
+++ src/components/interview-guide/index.js
@@ -2,10 +2,8 @@ import PropTypes from "prop-types";
 import withTraitify from "lib/with-traitify";
 import {Component} from "react";
 import ApolloClient from "apollo-boost";
-import {ApolloProvider} from "react-apollo";
-// import query from "graphql-queries/guide";
-// import query from "graphql-queries/guide-for-assessment";
-// import keysToCamel from "lib/helpers/casing";
+import {ApolloProvider, Query} from "react-apollo";
+import query from "graphql-queries/guide-for-assessment";
 
 const client = new ApolloClient({
   headers: {authorization: "Basic ZGhtZ3NnMWQ2MmJxcDhpb2tqaTEzZnVobWY6eA==", mode: "no-cors"},
@@ -17,15 +15,15 @@ class InterviewGuide extends Component {
   static propTypes = {
     assessment: PropTypes.shape({assessment_type: PropTypes.string})
   }
-  // variables={{assessment_id: "ba706c74-c256-4367-afa6-cfb84c5ebe22"}
   buildTypes(types) {
-    return types.map((type) => ({personalityType: type.personality_type}));
+    return types.map((type) => {
+      let personalityType = type.personality_type;
+      personalityType = {level: personalityType.level, id: personalityType.id};
+      return {personalityType};
+    });
   }
   render() {
     if(!this.props.assessment) { return null; }
-    if(this.props.assessment) {
-      console.log("waasss");
-    }
 
     const props = this.props.assessment;
     const assessment = {
@@ -33,12 +31,19 @@ class InterviewGuide extends Component {
       deckId: props.deck_id,
       personalityTypes: this.buildTypes(props.personality_types)
     };
-    console.log(assessment);
 
     return (
       <ApolloProvider client={client}>
         <div className="interview-guide-container">
-          ddd
+          <Query query={query} variables={{assessment}}>
+            {({loading, error, data}) => {
+              if(loading) return <p>Loading...</p>;
+              if(error) return <p>Error :(</p>;
+
+              console.log(data, "query data");
+              return <div>testing please wait a moment</div>;
+            }}
+          </Query>
         </div>
       </ApolloProvider>
     );
--- src/graphql-queries/guide-for-assessment.js
+++ src/graphql-queries/guide-for-assessment.js
@@ -1,73 +1,27 @@
 import gql from "graphql-tag";
 
-// try w full object -
-// then try other ideas
-export default gql`{
+export default gql`
   query GuideForAssessment($assessment: Assessment!) {
     guideForAssessment(assessment: $assessment, localeKey: "en-us") {
       id
       name
       deckId
+      competencies {
+        id
+        name
+        introduction
+        order
+        questionSequences {
+          id
+          name
+          questions {
+            id
+            text
+            adaptability
+            order
+          }
+        }
+      }
     }
   }
 `;
-
-// export default gql`{
-//   guideForAssessment(assessment: {
-//        id: "29b89abe-0be1-4be4-aa3b-43773ebe8ecf"
-//        deckId: "big-five"
-//        personalityTypes: [
-//         {
-//                      personalityType: {
-//             id: "f087aa70-14ea-4293-9327-cedf0e4ce1fa"
-//             level: "High"
-//           }
-//         },
-//         {
-//                      personalityType: {
-//             id: "462b8063-69f4-4b50-990b-8daee93e8616"
-//             level: "Medium"
-//           }
-//         },
-//         {
-//                      personalityType: {
-//             id: "2835d442-98be-49ff-9cf1-0e0ea577fa77"
-//             level: "Medium"
-//           }
-//         },
-//         {
-//                      personalityType: {
-//             id: "f702b455-2c0e-433c-a274-b01b68ae29ba"
-//             level: "Medium"
-//           }
-//         },
-//         {
-//                      personalityType: {
-//             id: "05596127-4fdd-4d22-8ae7-530d42d225eb"
-//             level: "Low"
-//           }
-//         }
-//       ]
-//     }, localeKey: "en-us") {
-//     id
-//     name
-//     deckId
-//     competencies {
-//       id
-//       name
-//       introduction
-//       order
-//       questionSequences {
-//         id
-//         name
-//         questions {
-//           id
-//           text
-//           adaptability
-//           order
-//         }
-//       }
-//     }
-//   }
-// }
-// `;

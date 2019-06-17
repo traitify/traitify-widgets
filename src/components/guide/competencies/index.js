ee2fea2015c3b26481a62d1bb3a6d8ad5cdfbff4
Jbur43 <jtburum@gmail.com>
2019-06-17 14:48:03 -0400

    questionSequences component

--- src/components/guide/competencies/index.js
+++ src/components/guide/competencies/index.js
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 import {Component} from "react";
-
+import QuestionSequences from "../question-sequences/index";
 // Maybe this should be functional
 class Competencies extends Component {
   static defaultProps = {competencies: null}
@@ -12,15 +12,12 @@ class Competencies extends Component {
       <div className="competency-container">
         {this.props.competencies.map((competency) => (
           <div className="competency" key={competency.id}>
-            <div className="name">
-              {competency.name}
+            <div className="name">{competency.name}</div>
+            <div className="introduction">{competency.introduction}</div>
+            <div className="question-sequences">
+              <QuestionSequences questionSequences={competency.questionSequences} />
             </div>
-            <div className="name">
-              {competency.introduction}
-            </div>
-            <div className="name">
-              {competency.name}
-            </div>
+            <br />
           </div>
         ))}
       </div>

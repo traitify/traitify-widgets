60de6a4f24ee8695935313150caed9e2dff7f3c8
Jbur43 <jtburum@gmail.com>
2019-06-24 21:59:02 -0400

    wip remove libs for queries

--- src/components/guide/index.js
+++ src/components/guide/index.js
@@ -1,7 +1,7 @@
 // import PropTypes from "prop-types";
 import withTraitify from "lib/with-traitify";
 import {Component} from "react";
-// import TraitifyClient from "lib/traitify-client";
+import TraitifyClient from "lib/traitify-client";
 import GraphQL from "graphql/gql-client";
 
 class Guide extends Component {
@@ -13,16 +13,15 @@ class Guide extends Component {
   //   })
   // }
   render() {
-    // const params = {
-    //   assessmentId: "4f2a3c1c-57e4-4a86-b59c-4ebbf7787a8a",
-    //   localeKey: "en-US"
-    // };
+    const params = {
+      assessmentId: "4f2a3c1c-57e4-4a86-b59c-4ebbf7787a8a",
+      localeKey: "en-US"
+    };
     const fields = ["deckId"];
     const graphql = new GraphQL();
-    graphql.toQuery(fields);
+    // graphql.toQuery(fields);
     // console.log(graphql);
-    // console.log(new TraitifyClient().graphqlQuery("/interview_guides/
-    // graphql", `{ guide(${graphql.toArgs(params)}) { ${graphql.toQuery(fields)} }}`));
+    console.log(new TraitifyClient().graphqlQuery("/interview_guides/graphql", `{ guide(${graphql.toArgs(params)}) { ${graphql.toQuery(fields)} }}`));
 
     return (
       <div className="guide-container">

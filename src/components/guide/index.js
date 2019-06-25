2128232fc5cb0db2ab61a116ef3630a1a77f78b2
Jbur43 <jtburum@gmail.com>
2019-06-24 22:10:51 -0400

    dynamic assessment id

--- src/components/guide/index.js
+++ src/components/guide/index.js
@@ -1,20 +1,21 @@
-// import PropTypes from "prop-types";
+import PropTypes from "prop-types";
 import withTraitify from "lib/with-traitify";
 import {Component} from "react";
 import TraitifyClient from "lib/traitify-client";
 import GraphQL from "graphql/gql-client";
 
 class Guide extends Component {
-  // static defaultProps = {assessmentID: null, traitify: null}
-  // static propTypes = {
-  //   assessmentID: PropTypes.string,
-  //   traitify: PropTypes.shape({
-  //     publicKey: PropTypes.string
-  //   })
-  // }
+  static defaultProps = {assessmentID: null, traitify: null}
+  static propTypes = {
+    assessmentID: PropTypes.string,
+    traitify: PropTypes.shape({
+      publicKey: PropTypes.string
+    })
+  }
   render() {
+    const {assessmentID} = this.props;
     const params = {
-      assessmentId: "4f2a3c1c-57e4-4a86-b59c-4ebbf7787a8a",
+      assessmentId: assessmentID,
       localeKey: "en-US"
     };
     const fields = [
@@ -22,8 +23,6 @@ class Guide extends Component {
       {competencies: ["id", "name", "introduction", "order", {questionSequences: ["id", "name", {questions: ["id", "text", "adaptability", "order"]}]}]}
     ];
     const graphql = new GraphQL();
-    // graphql.toQuery(fields);
-    // console.log(graphql);
     console.log(new TraitifyClient().graphqlQuery("/interview_guides/graphql", `{ guide(${graphql.toArgs(params)}) { ${graphql.toQuery(fields)} }}`));
 
     return (

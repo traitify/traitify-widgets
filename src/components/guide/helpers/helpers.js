ea54885b74371a1113885c0f1dbed55a8d68deb5
Jbur43 <jtburum@gmail.com>
2019-06-25 18:41:33 -0400

    selectbox on small screens

---
+++ src/components/guide/helpers/helpers.js
@@ -0,0 +1,3 @@
+export default function smallScreen() {
+  return window.innerWidth < 768;
+}
--- src/components/guide/index.js
+++ src/components/guide/index.js
@@ -4,8 +4,7 @@ import withTraitify from "lib/with-traitify";
 import {Component} from "react";
 import TraitifyClient from "lib/traitify-client";
 import GraphQL from "graphql/gql-client";
-// import Competencies from "./competencies/index";
-// <Competencies competencies={this.state.competencies} />
+import smallScreen from "./helpers/helpers"
 import style from "./style";
 
 class Guide extends Component {
@@ -28,7 +27,7 @@ class Guide extends Component {
     };
     const fields = [
       "deckId", "id", "name",
-      {competencies: ["id", "name", "introduction", "order", {questionSequences: ["id", "name", {questions: ["id", "text", "adaptability", "order"]}]}]}
+      {competencies: ["id", "name", "introduction", "order", {questionSequences: ["id", "name", {questions: ["id", "text", "adaptability", "order", "purpose"]}]}]}
     ];
     const graphql = new GraphQL();
     new TraitifyClient().graphqlQuery("/interview_guides/graphql", `{ guide(${graphql.toArgs(params)}) { ${graphql.toQuery(fields)} }}`)
@@ -38,39 +37,104 @@ class Guide extends Component {
         this.setState({competencies, displayedCompetency: competencies[0]});
       });
   }
-  displayedCompetency(competency) {
-    console.dir(competency.target.name);
+  displayCompetency(competency) {
+    competency = typeof(competency) === "string" ? competency : competency.target.value
     this.state.competencies.forEach((comp) => {
-      if(competency.target.name === comp.name) {
+      if(competency === comp.name) {
         this.setState({displayedCompetency: comp});
       }
     });
   }
+  questionObjectives(order) {
+    const questionOne = (
+      <div className="question-objectives">
+        <ul>
+          <li>Do they talk about a number of different ways of approaching the problem?</li>
+          <li>Are their ideas new, original or different in any way?</li>
+          <li>Do they link their suggestions with a broader (maybe more strategic) view of what could be achieved?</li>
+          <li>Generating options, being imaginative, playing with new ideas and taking a long term view should all be aspects of open-minded characters?</li>
+        </ul>
+      </div>
+    );
+    const questionTwo = (
+      <div className="question-objectives">
+        <h4>What are you trying to find out?</h4>
+        <ul>
+          <li>How comfortable are they working in a step-by-step manner?</li>
+          <li>Do they seem to find it difficult?</li>
+          <li>Adipisicing fugiat asperiores eius eum commodi unde, necessitatibus atque Ullam ipsa deserunt incidunt fuga harum Voluptate at illo autem atque maxime. Debitis perspiciatis omnis quae incidunt ducimus Enim earum fuga?</li>
+          <li>Adipisicing fugiat asperiores eius eum commodi unde, necessitatibus atque Ullam ipsa deserunt incidunt fuga harum Voluptate at illo autem atque maxime. Debitis perspiciatis omnis quae incidunt ducimus Enim earum fuga?</li>
+        </ul>
+      </div>
+    );
+    const questionThree = (
+      <div className="question-objectives">
+        <h4>What are you trying to find out?</h4>
+        <ul>
+          <li>Adipisicing fugiat asperiores eius eum commodi unde, necessitatibus atque Ullam ipsa deserunt incidunt fuga harum Voluptate at illo autem atque maxime. Debitis perspiciatis omnis quae incidunt ducimus Enim earum fuga?</li>
+          <li>Adipisicing fugiat asperiores eius eum commodi unde, necessitatibus atque Ullam ipsa deserunt incidunt fuga harum Voluptate at illo autem atque maxime. Debitis perspiciatis omnis quae incidunt ducimus Enim earum fuga?</li>
+          <li>Adipisicing fugiat asperiores eius eum commodi unde, necessitatibus atque Ullam ipsa deserunt incidunt fuga harum Voluptate at illo autem atque maxime. Debitis perspiciatis omnis quae incidunt ducimus Enim earum fuga?</li>
+          <li>Adipisicing fugiat asperiores eius eum commodi unde, necessitatibus atque Ullam ipsa deserunt incidunt fuga harum Voluptate at illo autem atque maxime. Debitis perspiciatis omnis quae incidunt ducimus Enim earum fuga?</li>
+        </ul>
+      </div>
+    );
+    switch(order) {
+      case 1: return questionOne;
+      case 2: return questionTwo;
+      case 3: return questionThree;
+    }
+  }
+  splitAndMap(purpose) {
+    purpose.split("")
+  }
+  adaptability(adaptability) {
+    if(!adaptability) { return }
+    return(
+      <div>
+        <h4>Have they developed a coping style?</h4>
+        <div>{adaptability}</div>
+      </div>
+    )
+  }
+  selectBoxOrTabs() {
+    if(smallScreen()) {
+      return (
+        <select value={this.state.displayedCompetency.name} onChange={(e) => this.displayCompetency(e)}>
+          {this.state.competencies.map((competency, index) => (<option value={competency.name}>{competency.name}</option>))}
+        </select>
+      )
+    } else {
+      return (
+        <ul className={style.tabs}>
+          {this.state.competencies.map((competency, index) => (
+            <li
+              className={style.tabActive}
+              role="link"
+              tabIndex={0}
+              onKeyPress={(e) => this.displayCompetency(competency.name)}
+              onClick={(e) => this.displayCompetency(competency.name)}
+              name={competency.name}
+            >
+              <a href={`#tab-${index}`} value={competency.name}>
+                <img src="//placehold.it/100x100" alt="Type Name badge" value={competency.name} />
+                <br />
+                {competency.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      )
+    }
+  }
   render() {
-    if(this.state.competencies.length === 0) { this.setCompetencies(); return null; }
+    console.log(smallScreen());
+    if(this.state.competencies.length === 0) { this.setCompetencies(); return <div />; }
     const {displayedCompetency} = this.state;
 
     return (
       <div className={style.tabsContainer}>
         <div className={style.tabContainer}>
-          <ul className={style.tabs}>
-            {this.state.competencies.map((competency, index) => (
-              <li
-                className={style.tabActive}
-                role="link"
-                tabIndex={0}
-                onKeyPress={(e) => this.displayedCompetency(e)}
-                onClick={(e) => this.displayedCompetency(e)}
-                name={competency.name}
-              >
-                <a href={`#tab-${index}`} name={competency.name}>
-                  <img src="//placehold.it/100x100" alt="Type Name badge" name={competency.name} />
-                  <br />
-                  {competency.name}
-                </a>
-              </li>
-            ))}
-          </ul>
+          {this.selectBoxOrTabs()}
         </div>
 
         <div className={style.tabsContent}>
@@ -87,14 +151,11 @@ class Guide extends Component {
                 {sequence.questions.map((question) => (
                   <div className="questions">
                     <h3>{`Question ${question.order}`}</h3>
+
                     <p>{question.text}</p>
                     <h4>What are you trying to find out?</h4>
-                    <ul>
-                      <li>Adipisicing fugiat asperiores eius eum commodi unde, necessitatibus atque Ullam ipsa deserunt incidunt fuga harum Voluptate at illo autem atque maxime. Debitis perspiciatis omnis quae incidunt ducimus Enim earum fuga?</li>
-                      <li>Adipisicing fugiat asperiores eius eum commodi unde, necessitatibus atque Ullam ipsa deserunt incidunt fuga harum Voluptate at illo autem atque maxime. Debitis perspiciatis omnis quae incidunt ducimus Enim earum fuga?</li>
-                      <li>Adipisicing fugiat asperiores eius eum commodi unde, necessitatibus atque Ullam ipsa deserunt incidunt fuga harum Voluptate at illo autem atque maxime. Debitis perspiciatis omnis quae incidunt ducimus Enim earum fuga?</li>
-                      <li>Adipisicing fugiat asperiores eius eum commodi unde, necessitatibus atque Ullam ipsa deserunt incidunt fuga harum Voluptate at illo autem atque maxime. Debitis perspiciatis omnis quae incidunt ducimus Enim earum fuga?</li>
-                    </ul>
+                    <div>{question.purpose}</div>
+                    {this.adaptability(question.adaptability)}
                   </div>
                 ))}
               </div>

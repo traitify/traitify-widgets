0766d3ed3ac6e1fdd5c474752aa4882f83df82d2
Jbur43 <jtburum@gmail.com>
2019-06-25 09:27:43 -0400

    new query setting state

--- src/components/guide/index.js
+++ src/components/guide/index.js
@@ -3,8 +3,13 @@ import withTraitify from "lib/with-traitify";
 import {Component} from "react";
 import TraitifyClient from "lib/traitify-client";
 import GraphQL from "graphql/gql-client";
+import Competencies from "./competencies/index";
 
 class Guide extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {competencies: []};
+  }
   static defaultProps = {assessmentID: null, traitify: null}
   static propTypes = {
     assessmentID: PropTypes.string,
@@ -12,7 +17,7 @@ class Guide extends Component {
       publicKey: PropTypes.string
     })
   }
-  render() {
+  setCompetencies() {
     const {assessmentID} = this.props;
     const params = {
       assessmentId: assessmentID,
@@ -23,11 +28,18 @@ class Guide extends Component {
       {competencies: ["id", "name", "introduction", "order", {questionSequences: ["id", "name", {questions: ["id", "text", "adaptability", "order"]}]}]}
     ];
     const graphql = new GraphQL();
-    console.log(new TraitifyClient().graphqlQuery("/interview_guides/graphql", `{ guide(${graphql.toArgs(params)}) { ${graphql.toQuery(fields)} }}`));
+    new TraitifyClient().graphqlQuery("/interview_guides/graphql", `{ guide(${graphql.toArgs(params)}) { ${graphql.toQuery(fields)} }}`)
+      .then((response) => {
+        const {competencies} = response.data.guide;
+        this.setState({competencies});
+      });
+  }
+  render() {
+    if(this.state.competencies.length === 0) { this.setCompetencies(); return null; }
 
     return (
       <div className="guide-container">
-        testing
+        <Competencies competencies={this.state.competencies} />
       </div>
     );
   }

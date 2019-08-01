6a4b6bec72a4ec1c23ce1fa54c90f149467c66a4
Jbur43 <jtburum@gmail.com>
2019-08-01 09:31:54 -0500

    casing

--- src/lib/i18n-data.js
+++ src/lib/i18n-data.js
@@ -8,7 +8,7 @@ import jaJP from "lib/i18n-data/ja_jp.json";
 import nlNL from "lib/i18n-data/nl_nl.json";
 import noNO from "lib/i18n-data/no_no.json";
 import ptBR from "lib/i18n-data/pt_br.json";
-import ruRu from "lib/i18n-data/ru_ru.json";
+import ruRU from "lib/i18n-data/ru_ru.json";
 import svSE from "lib/i18n-data/sv_se.json";
 import zhCN from "lib/i18n-data/zh_cn.json";
 
@@ -24,7 +24,7 @@ export default {
   "nl-nl": nlNL,
   "no-no": noNO,
   "pt-br": ptBR,
-  "ru-ru": ruRu,
+  "ru-ru": ruRU,
   "sv-se": svSE,
   "zh-cn": zhCN
 };

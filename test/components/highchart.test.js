d847f7486a858673a11aa47745a5dd4f23406b48
Jbur43 <jtburum@gmail.com>
2020-01-31 12:35:55 -0600

    highchart pruning and test

---
+++ test/components/highchart.test.js
@@ -0,0 +1,31 @@
+import Component from "components/highchart";
+import ComponentHandler from "support/component-handler";
+
+jest.mock("highcharts", () => (() => (<div className="mock">HighCharts</div>)));
+jest.mock("highcharts-react-official", () => (() => (<div className="mock">HighchartsReactOfficial</div>)));
+
+describe("HighChart", () => {
+  let props;
+
+  beforeEach(() => {
+    props = {
+      chart: {marginRight: 50, marginLeft: 50},
+      legend: {layout: "horizontal", align: "center", width: 500, itemStyle: {width: 300}, margin: 30},
+      series: [{
+        name: "Engineer Traits",
+        data: [{y: 80, description: "forcefully pursuing a goal", name: "Aggressive"}]
+      }, {
+        name: "Your Traits",
+        data: [{y: 78, description: "forcefully pursuing a goal", name: "Aggressive"}]
+      }],
+      type: "column",
+      xAxis: {type: "category"}
+    };
+  });
+
+  it("renders column highchart", () => {
+    const component = new ComponentHandler(<Component {...props} />);
+
+    expect(component.tree).toMatchSnapshot();
+  });
+});

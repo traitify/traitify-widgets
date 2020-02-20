390440a3e7f7963033e71036ed436c1b6114a3d5
Jbur43 <jtburum@gmail.com>
2020-02-20 09:26:45 -0600

    shuffle importa and props order

--- test/components/results/candidate-results.test.js
+++ test/components/results/candidate-results.test.js
@@ -1,6 +1,7 @@
 import Component from "components/results/candidate-results";
 import ComponentHandler from "support/component-handler";
 
+jest.mock("components/guide", () => (() => (<div className="mock">Guide</div>)));
 jest.mock("components/results/candidate-results/personality-archetype", () => (() => (<div className="mock">Personality Archetype</div>)));
 jest.mock("components/results/candidate-results/personality-details", () => (() => (<div className="mock">Personality Details</div>)));
 jest.mock("components/results/candidate-results/personality-dimensions", () => (() => (<div className="mock">Personality Dimensions</div>)));
@@ -12,7 +13,6 @@ describe("CandidateResults", () => {
 
   beforeEach(() => {
     props = {
-      followGuide: jest.fn().mockName("followGuide").mockReturnValue(true),
       isReady: jest.fn().mockName("isReady").mockReturnValue(true),
       options: {},
       translate: jest.fn().mockName("translate").mockImplementation((value, options = {}) => `${value}, ${options}`),
@@ -34,6 +34,7 @@ describe("CandidateResults", () => {
 
   it("renders results in thirdPerson", () => {
     props.options.perspective = "thirdPerson";
+    props.followGuide = jest.fn().mockName("followGuide").mockReturnValue(true);
     const component = new ComponentHandler(<Component {...props} />);
 
     expect(component.tree).toMatchSnapshot();

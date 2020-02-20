7dc0a8d5793b80cf42896c930a38357c78f036a5
Jbur43 <jtburum@gmail.com>
2020-02-19 20:43:23 -0600

    add-guide-to-candidate-results for third person

--- src/components/results/candidate-results/index.js
+++ src/components/results/candidate-results/index.js
@@ -1,16 +1,22 @@
+import PropTypes from "prop-types";
 import {Component} from "react";
 import PersonalityArchetype from "./personality-archetype";
+import Guide from "../../guide";
 import PersonalityDetails from "./personality-details";
 import PersonalityDimensions from "./personality-dimensions";
 import PersonalityTraits from "../dimension-based-results/personality-traits";
 
 export default class CandidateResults extends Component {
+  static propTypes = {
+    options: PropTypes.shape({perspective: PropTypes.string}).isRequired
+  }
   render() {
     return (
       <section>
         <PersonalityArchetype {...this.props} />
         <PersonalityDimensions {...this.props} />
         <PersonalityDetails {...this.props} />
+        {this.props.options.perspective === "thirdPerson" && <Guide {...this.props} />}
         <PersonalityTraits {...this.props} />
       </section>
     );

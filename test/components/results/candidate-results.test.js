028ff82614dbfff5574f12eefad8ce7a0fe590d7
Jack Burum <jtburum@gmail.com>
2020-02-20 14:37:20 -0600

    Add guide to candidate results (#188)
    * add-guide-to-candidate-results for third person
    
    * add and update test
    
    * shuffle importa and props order
    
    * use getOption
    
    * withTraitify
    
    * version bump

--- test/components/results/candidate-results.test.js
+++ test/components/results/candidate-results.test.js
@@ -1,6 +1,7 @@
 import Component from "components/results/candidate-results";
 import ComponentHandler from "support/component-handler";
 
+jest.mock("components/guide", () => (() => (<div className="mock">Guide</div>)));
 jest.mock("components/results/candidate-results/personality-archetype", () => (() => (<div className="mock">Personality Archetype</div>)));
 jest.mock("components/results/candidate-results/personality-details", () => (() => (<div className="mock">Personality Details</div>)));
 jest.mock("components/results/candidate-results/personality-dimensions", () => (() => (<div className="mock">Personality Dimensions</div>)));
@@ -8,8 +9,33 @@ jest.mock("components/results/dimension-based-results/personality-traits", () =>
 jest.mock("lib/with-traitify", () => ((value) => value));
 
 describe("CandidateResults", () => {
-  it("renders results", () => {
-    const component = new ComponentHandler(<Component />);
+  let props;
+
+  beforeEach(() => {
+    props = {
+      getOption: jest.fn().mockName("getOption").mockReturnValue("firstPerson"),
+      isReady: jest.fn().mockName("isReady").mockReturnValue(true),
+      options: {},
+      translate: jest.fn().mockName("translate").mockImplementation((value, options = {}) => `${value}, ${options}`),
+      ui: {
+        current: {},
+        off: jest.fn().mockName("off"),
+        on: jest.fn().mockName("on"),
+        trigger: jest.fn().mockName("trigger")
+      }
+    };
+  });
+
+  it("renders results in firstPerson", () => {
+    const component = new ComponentHandler(<Component {...props} />);
+
+    expect(component.tree).toMatchSnapshot();
+  });
+
+  it("renders results in thirdPerson", () => {
+    props.getOption = jest.fn().mockName("getOption").mockReturnValue("thirdPerson");
+    props.followGuide = jest.fn().mockName("followGuide").mockReturnValue(true);
+    const component = new ComponentHandler(<Component {...props} />);
 
     expect(component.tree).toMatchSnapshot();
   });

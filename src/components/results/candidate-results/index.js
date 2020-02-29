b81d0ca66694992157d44a3c9712471bddc7782e
Jbur43 <jtburum@gmail.com>
2020-02-29 12:48:55 -0600

    headings on hmv

--- src/components/results/candidate-results/index.js
+++ src/components/results/candidate-results/index.js
@@ -1,7 +1,12 @@
+import {
+  faThLarge
+} from "@fortawesome/free-solid-svg-icons";
 import PropTypes from "prop-types";
+import Icon from "lib/helpers/icon";
 import {Component} from "react";
 import withTraitify from "lib/with-traitify";
 import Guide from "../../guide";
+import style from "./style";
 import PersonalityArchetype from "./personality-archetype";
 import PersonalityDetails from "./personality-details";
 import PersonalityDimensions from "./personality-dimensions";
@@ -9,15 +14,27 @@ import PersonalityTraits from "../dimension-based-results/personality-traits";
 
 class CandidateResults extends Component {
   static propTypes = {
-    getOption: PropTypes.func.isRequired
+    getOption: PropTypes.func.isRequired,
+    translate: PropTypes.func.isRequired
   }
   render() {
+    const {getOption, translate} = this.props;
+
     return (
-      <section>
+      <section className={style.container}>
+        <p className={style.heading}><Icon icon={faThLarge} /> {translate("personality_type")}</p>
         <PersonalityArchetype {...this.props} />
+        <p className={style.heading}><Icon icon={faThLarge} /> {translate("personality_details")}</p>
         <PersonalityDimensions {...this.props} />
+        <p className={style.lessBottomMargin}><Icon icon={faThLarge} /> {translate("personality_advice")}</p>
         <PersonalityDetails {...this.props} />
-        {this.props.getOption("perspective") === "thirdPerson" && <Guide {...this.props} />}
+        {getOption("perspective") === "thirdPerson" && (
+          <div>
+            <p className={style.heading}><Icon icon={faThLarge} /> {translate("interview_guide_heading")}</p>
+            <Guide {...this.props} />
+          </div>
+        )}
+        <p className={style.topPadding}><Icon icon={faThLarge} /> {translate("personality_breakdown")}</p>
         <PersonalityTraits {...this.props} />
       </section>
     );

e8735a7d45664ed94ce8b3423de57f3911fd1505
Katie Harron <katie@pibby.com>
2020-04-16 10:53:29 -0500

    Success Skills for Big 5

--- src/components/results/edm-results/personality-skills/index.js
+++ src/components/results/edm-results/personality-skills/index.js
@@ -25,7 +25,59 @@ class PersonalitySkills extends Component {
 
     return (
       <div className={style.container}>
-        Skills
+        <ul className={style.skillsTabs}>
+          <div className={style.featuredSkill}>Featured Skill</div>
+          <li className={style.active}>
+            <button>
+              <img src="https://cdn.traitify.com/images/big5_home.png" alt="Working From Home Tips" className={style.skillImage} />
+              <div className={style.skillName}>Working From Home Tips</div>
+            </button>
+          </li>
+          <li>
+            <button>
+              <img src="https://cdn.traitify.com/images/big5_stress.png" alt="Dealing With Stress" className={style.skillImage} />
+              <div className={style.skillName}>Dealing With Stress</div>
+            </button>
+          </li>
+          <li>
+            <button>
+              <img src="https://cdn.traitify.com/images/big5_communication.png" alt="Communication Tips" className={style.skillImage} />
+              <div className={style.skillName}>Communication Tips</div>
+            </button>
+          </li>
+          <li>
+            <button>
+              <img src="https://cdn.traitify.com/images/big5_teamwork.png" alt="Teamwork" className={style.skillImage} />
+              <div className={style.skillName}>Teamwork</div>
+            </button>
+          </li>
+          <li>
+            <button>
+              <img src="https://cdn.traitify.com/images/big5_motivation.png" alt="Self Motivation" className={style.skillImage} />
+              <div className={style.skillName}>Self Motivation</div>
+            </button>
+          </li>
+        </ul>
+
+        <div className={style.skillsTabBox}>
+          <div className={style.formSelect}>
+            <select>
+              <option>Working From Home Tips</option>
+              <option>Dealing With Stress</option>
+              <option>Communication Tips</option>
+              <option>Teamwork</option>
+              <option>Self Motivation</option>
+            </select>
+          </div>
+          <h3>Here are some useful tips that will help your performance while working from home</h3>
+          <ul className={style.skills}>
+            <li>Find ways to innovate. Does a remote workforce have needs that your company could fill?</li>
+            <li>Check in on colleagues to help them meet daily goals if needed.</li>
+            <li>Change your workspace from time to time. Work while standing to keep your energy up.</li>
+            <li>Send supportive messages to coworkers to help ensure everyone is motivated and productive.</li>
+            <li>You're naturally calm and steady. Be positive during video chats.</li>
+          </ul>
+        </div>
       </div>
     );
   }

f10790ac08394af21e26ca8c758ecc2958f6aa35
Tom Prats <tprats108@gmail.com>
2020-05-21 16:21:22 -0400

    Alpha Version

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ export Loading from "./loading";
 export Results from "./results";
 export CandidateResults from "./results/candidate-results";
 export DimensionBasedResults from "./results/dimension-based-results";
+export EmployeeResults from "./results/employee-results";
 export FinancialRiskResults from "./results/financial-risk-results";
 export Guide from "./results/guide";
 export TypeBasedResults from "./results/type-based-results";
@@ -10,5 +11,6 @@ export SlideDeck from "./slide-deck";
 
 export * as CandidateComponents from "./results/candidate-results/components";
 export * as DimensionComponents from "./results/dimension-based-results/components";
+export * as EmployeeComponents from "./results/employee-results/components";
 export * as FinancialRiskComponents from "./results/financial-risk-results/components";
 export * as TypeComponents from "./results/type-based-results/components";

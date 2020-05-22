573349dadbf654bbe5ca4464d8be2e8919120e93
Tom Prats <tprats108@gmail.com>
2020-05-21 21:52:53 -0400

    Retry Peer Dependency

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -13,8 +13,18 @@ let config = {
   devtool: "source-map",
   entry: ["./index.js"],
   externals : {
-    "react": "React",
-    "react-dom": "ReactDOM"
+    "react": {
+      root: "React",
+      commonjs2: "react",
+      commonjs: "react",
+      amd: "react"
+    },
+    "react-dom": {
+      root: "ReactDOM",
+      commonjs2: "react-dom",
+      commonjs: "react-dom",
+      amd: "react-dom"
+    }
   },
   mode: env,
   module: {

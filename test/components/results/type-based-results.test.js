d38f9d25a7b2a63a727dc38ec143cf297484f2a6
Jbur43 <jtburum@gmail.com>
2020-10-23 14:16:55 -0400

    add test for type based results

--- test/components/results/type-based-results.test.js
+++ test/components/results/type-based-results.test.js
@@ -5,11 +5,27 @@ jest.mock("components/results/type-based-results/personality-base", () => (() =>
 jest.mock("components/results/type-based-results/personality-details", () => (() => (<div className="mock">Personality Details</div>)));
 jest.mock("components/results/type-based-results/personality-traits", () => (() => (<div className="mock">Personality Traits</div>)));
 jest.mock("components/results/type-based-results/personality-types", () => (() => (<div className="mock">Personality Types</div>)));
-jest.mock("lib/with-traitify");
+jest.mock("lib/with-traitify", () => ((value) => value));
 
 describe("TypeBasedResults", () => {
+  let props;
+
+  beforeEach(() => {
+    props = {
+      getOption: jest.fn().mockName("getOption").mockReturnValue(false),
+      translate: jest.fn().mockName("translate").mockImplementation((value, options = {}) => `${value}, ${options}`)
+    };
+  });
+
   it("renders results", () => {
-    const component = new ComponentHandler(<Component />);
+    const component = new ComponentHandler(<Component {...props} />);
+
+    expect(component.tree).toMatchSnapshot();
+  });
+
+  it("renders results with headers", () => {
+    props.getOption = jest.fn().mockName("getOption").mockReturnValue(true);
+    const component = new ComponentHandler(<Component {...props} />);
 
     expect(component.tree).toMatchSnapshot();
   });

19ac7e895ef6f99c6a998977cac3c8c1d789743e
Justin Xu <justinxu012@gmail.com>
2022-06-08 11:13:19 -0400

    added more tests

--- test/components/paradox/results/personality/career/container.test.js
+++ test/components/paradox/results/personality/career/container.test.js
@@ -1,11 +1,13 @@
 import Component from "components/paradox/results/personality/career/container";
 import {mockProps, mockUI} from "support/helpers";
 import careers from "support/json/careers.json";
+import dimensionBasedResults from "support/json/assessment/dimension-based.json";
 import ComponentHandler from "support/component-handler";
 import I18n from "lib/i18n";
 import {act} from "react-test-renderer";
 
 jest.mock("lib/with-traitify", () => ((value) => value));
+jest.mock("components/highchart", () => (() => <div className="mock">HighCharts</div>));
 
 describe("Paradox.CareerContainer", () => {
   let component;
@@ -16,6 +18,7 @@ describe("Paradox.CareerContainer", () => {
       ...mockProps(["followBenchmark", "followGuide", "getOption", "isReady", "setElement", "translate", "traitify"]),
       locale: "en",
       i18n: I18n,
+      assessment: dimensionBasedResults,
       assessmentID: "test",
       options: null
     };
@@ -35,6 +38,16 @@ describe("Paradox.CareerContainer", () => {
     props.traitify.get = () => Promise.resolve([]);
     await act(async() => {
       component = new ComponentHandler(<Component assessmentID={props.assessmentID} {...props} />);
+    });
+    expect(component.tree).toMatchSnapshot();
+  });
+
+  it("modal opens", async() => {
+    await act(async() => {
+      component = new ComponentHandler(<Component assessmentID={props.assessmentID} {...props} />);
+    });
+    act(() => {
+      component.instance.findAllByType("button")[2].props.onClick();
     });
     expect(component.tree).toMatchSnapshot();
   });

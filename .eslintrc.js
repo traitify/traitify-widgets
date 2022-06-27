ce3317f69e8e9fbc9ca7505da5f30f609ec0a5ff
Tomify <tprats108@gmail.com>
2022-06-27 15:58:30 -0400

    Updated guide

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,5 +5,10 @@ module.exports = {
   extends: ["traitify"],
   globals: {
     XDomainRequest: true
+  },
+  settings: {
+    "import/resolver": {
+      webpack: {config: "webpack.config.js"}
+    }
   }
 };

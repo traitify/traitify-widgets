56a3d07db8ee6cbac8899fd01529f759f13cc48f
Jbur43 <jtburum@gmail.com>
2022-09-20 18:00:38 -0400

    return urls

--- src/lib/traitify-client.js
+++ src/lib/traitify-client.js
@@ -2,7 +2,7 @@ import {toQueryString} from "lib/helpers/object";
 
 export default class TraitifyClient {
   constructor() {
-    this.host = "https://api.stag.awse.traitify.com";
+    this.host = "https://api.traitify.com";
     this.version = "v1";
     this.oldIE = typeof XDomainRequest !== "undefined";
   }

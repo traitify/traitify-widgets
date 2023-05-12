dd1111b88bf8eb4c6d186ecec1bdb1c8d2a0ce51
Justin Xu <justinxu012@gmail.com>
2023-05-12 10:17:50 -0400

    separated job org options away from individual careers

--- src/lib/hooks/use-careers.js
+++ src/lib/hooks/use-careers.js
@@ -1,11 +1,12 @@
 import {useCallback, useEffect, useState} from "react";
 import {useRecoilValue, useSetRecoilState} from "recoil";
-import {careersParamsState, careersState} from "lib/recoil";
+import {careersParamsState, careersState, jobOptionsState} from "lib/recoil";
 
 export default function useCareers() {
   const request = useRecoilValue(careersState);
   const setParams = useSetRecoilState(careersParamsState);
   const [state, setState] = useState({moreRecords: true, params: {}, records: []});
+  const setJobOptions = useSetRecoilState(jobOptionsState);
 
   const getNextPage = useCallback(() => (
     setParams(({page, ...currentParams}) => ({...currentParams, page: page + 1}))
@@ -15,13 +16,15 @@ export default function useCareers() {
     if(!request.records) { return; }
 
     setState({
-      moreRecords: request.records.length > 0
-        && (request.records.length % request.params.careers_per_page) === 0,
+      moreRecords: request.records.careers.length > 0
+        && (request.records.careers.length % request.params.careers_per_page) === 0,
       params: request.params,
       records: request.params.page === 1
-        ? request.records
-        : [...state.records, ...request.records]
+        ? request.records.careers
+        : [...state.records, ...request.records.careers]
     });
+
+    setJobOptions(request?.records?.jobOptions || {});
   }, [request.records]);
 
   return {...request, ...state, getNextPage};

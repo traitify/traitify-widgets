b79fc6fd1f7e1489a98e657badf6b1fea255c2fc
Tomify <tprats108@gmail.com>
2024-01-24 14:44:58 -0500

    Added new locales

--- src/lib/i18n-data.js
+++ src/lib/i18n-data.js
@@ -7,15 +7,20 @@ import esUS from "lib/i18n-data/es-us.json";
 import frCA from "lib/i18n-data/fr-ca.json";
 import frFR from "lib/i18n-data/fr-fr.json";
 import htUS from "lib/i18n-data/ht-us.json";
+import idID from "lib/i18n-data/id-id.json";
 import itIT from "lib/i18n-data/it-it.json";
 import jaJP from "lib/i18n-data/ja-jp.json";
+import koKR from "lib/i18n-data/ko-kr.json";
 import nlNL from "lib/i18n-data/nl-nl.json";
 import noNO from "lib/i18n-data/no-no.json";
 import ptBR from "lib/i18n-data/pt-br.json";
 import ptPT from "lib/i18n-data/pt-pt.json";
 import ruRU from "lib/i18n-data/ru-ru.json";
 import svSE from "lib/i18n-data/sv-se.json";
+import thTH from "lib/i18n-data/th-th.json";
+import viVN from "lib/i18n-data/vi-vn.json";
 import zhCN from "lib/i18n-data/zh-cn.json";
+import zhTW from "lib/i18n-data/zh-tw.json";
 
 export default {
   "de-de": {data: deDE, name: "German"},
@@ -28,13 +33,18 @@ export default {
   "fr-fr": {data: frFR, name: "French (France)"},
   "fr-us": {data: frCA, name: "French"},
   "ht-us": {data: htUS, name: "Creole"},
+  "id-id": {data: idID, name: "Indonesian"},
   "it-it": {data: itIT, name: "Italian"},
   "ja-jp": {data: jaJP, name: "Japanese"},
+  "ko-kr": {data: koKR, name: "Korean"},
   "nl-nl": {data: nlNL, name: "Dutch"},
   "no-no": {data: noNO, name: "Norwegian"},
   "pt-br": {data: ptBR, name: "Portuguese (Brazil)"},
   "pt-pt": {data: ptPT, name: "Portuguese"},
   "ru-ru": {data: ruRU, name: "Russian"},
   "sv-se": {data: svSE, name: "Swedish"},
-  "zh-cn": {data: zhCN, name: "Chinese"}
+  "th-th": {data: thTH, name: "Thai"},
+  "vi-vn": {data: viVN, name: "Vietnamese"},
+  "zh-cn": {data: zhCN, name: "Chinese (Simplified)"},
+  "zh-tw": {data: zhTW, name: "Chinese (Traditional)"}
 };

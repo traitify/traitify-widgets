be1273781f69300f75ac7b009817ac604f976ace
Tomify <tprats108@gmail.com>
2024-03-08 17:48:18 -0500

    Added labels from the benchmark

--- src/components/results/recommendation/chart/index.js
+++ src/components/results/recommendation/chart/index.js
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 import {useMemo} from "react";
 import reverse from "lib/common/array/reverse";
 import times from "lib/common/array/times";
-import {createColumns} from "lib/common/combine-data";
+import {createColumns, ranksFromBenchmark} from "lib/common/combine-data";
 import dig from "lib/common/object/dig";
 import useComponentEvents from "lib/hooks/use-component-events";
 import useDisabledComponent from "lib/hooks/use-disabled-component";
@@ -10,9 +10,8 @@ import useBenchmark from "lib/hooks/use-benchmark";
 import useGuide from "lib/hooks/use-guide";
 import useResults from "lib/hooks/use-results";
 import useSetting from "lib/hooks/use-setting";
+import useTranslate from "lib/hooks/use-translate";
 import style from "./style.scss";
-
-const colors = {high: "#29B770", low: "#EF615E", medium: "#FFCC3B", other: "black"};
 
 function RecommendationChart({combined}) {
   const benchmark = useBenchmark();
@@ -27,6 +26,8 @@ function RecommendationChart({combined}) {
 
     return createColumns({benchmark, guide, order: "types", types});
   }, [benchmark, guide, results]);
+  const ranks = useMemo(() => ranksFromBenchmark(benchmark), [benchmark]);
+  const translate = useTranslate();
 
   useComponentEvents("RecommendationChart");
 
@@ -34,43 +35,33 @@ function RecommendationChart({combined}) {
   if(disabled) { return null; }
   if(data.length === 0) { return null; }
 
-  const length = Math.max(...data.map(({data: points}) => points.length));
-  const colorFromRank = (rank) => {
-    if(rank === "low") { return benchmark ? benchmark.hexColorLow : colors.low; }
-    if(rank === "medium") { return benchmark ? benchmark.hexColorMedium : colors.medium; }
-    if(rank === "high") { return benchmark ? benchmark.hexColorHigh : colors.high; }
-
-    return colors.other;
-  };
-
-  let ranks = dig(benchmark, "resultRankings") || [];
-  ranks = [...ranks].sort((a, b) => ((a.maxScore < b.maxScore) ? 1 : -1));
+  const scaleLength = Math.max(...data.map(({data: points}) => points.length));
 
   return (
     <div className={[style.container, combined && style.combined].filter(Boolean).join(" ")}>
       <div className={style.ranks}>
-        {ranks.map(({description, visualHex, id}) => (
-          <div key={id} className={style.chartLegendContainer}>
-            <div className={style.chartLegendColor} style={{background: visualHex}} />
-            <div>{description}</div>
+        {ranks.map(({color, label, rank}) => (
+          <div key={rank} className={style.chartLegendContainer}>
+            <div className={style.chartLegendColor} style={{background: color}} />
+            <div>{label || translate(`level.${rank}`)}</div>
           </div>
         ))}
       </div>
       <div className={style.vertical}>
         <div className={style.scale}>
-          {times(length).map((index) => (
+          {times(scaleLength).map((index) => (
             <div key={index} className={style.label}>
-              <span>{length - index}</span>
+              <span>{scaleLength - index}</span>
             </div>
           ))}
         </div>
         {data.map(({competency, data: points, type}) => (
           <div key={type.id} className={style.column}>
-            {reverse(points).map(({active, rank}, index) => (
+            {reverse(points).map(({active, color, rank}, index) => (
               <div
                 key={index} /* eslint-disable-line react/no-array-index-key */
                 className={[active && style.active, style[rank]].filter(Boolean).join(" ")}
-                style={{background: colorFromRank(rank)}}
+                style={{background: color}}
               />
             ))}
             <div className={style.name}>{competency.name}</div>
@@ -85,18 +76,18 @@ function RecommendationChart({combined}) {
           <div className={style.grid}>
             {data.map(({data: points, type}) => (
               <div key={type.id} className={style.row}>
-                {points.map(({active, rank}, index) => (
+                {points.map(({active, color, rank}, index) => (
                   <div
                     key={index} /* eslint-disable-line react/no-array-index-key */
                     className={[active && style.active, style[rank]].filter(Boolean).join(" ")}
-                    style={{background: colorFromRank(rank)}}
+                    style={{background: color}}
                   />
                 ))}
               </div>
             ))}
           </div>
           <div className={style.scale}>
-            {times(length).map((index) => <div key={index}>{index + 1}</div>)}
+            {times(scaleLength).map((index) => <div key={index}>{index + 1}</div>)}
           </div>
         </div>
       </div>

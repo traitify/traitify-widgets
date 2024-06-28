99fa9ebd52d6678caaa26de838521dad93149eef
Quang Nguyen <quang.nguyenduy@paradox.ai>
2024-06-28 09:47:01 +0700

    fix test and add test

--- test/components/results/feedback/feedback.test.js
+++ test/components/results/feedback/feedback.test.js
@@ -1,8 +1,10 @@
 import Component from "components/results/feedback";
 import ComponentHandler from "support/component-handler";
-import {mockAssessment, mockUserCompletedFeedback} from "support/container/http";
+import {mockAssessment, mockFeedbackSurvey, mockUserCompletedFeedback} from "support/container/http";
+import {mockOption} from "support/container/options";
 import useContainer from "support/hooks/use-container";
 import assessment from "support/json/assessment/dimension-based.json";
+import feedbackSurvey from "support/json/feedback-survey.json";
 
 describe("Results.Feedback", () => {
   let component;
@@ -12,6 +14,8 @@ describe("Results.Feedback", () => {
   it("renders component", async() => {
     mockAssessment(assessment, {id: assessment.id});
     mockUserCompletedFeedback(assessment.id);
+    mockFeedbackSurvey(feedbackSurvey, assessment.deck_id);
+    mockOption("perspective", "firstPerson");
 
     component = await ComponentHandler.setup(Component);
 
@@ -21,6 +25,19 @@ describe("Results.Feedback", () => {
   it("renders empty when user has completed", async() => {
     mockAssessment(assessment, {id: assessment.id});
     mockUserCompletedFeedback(assessment.id, true);
+    mockFeedbackSurvey(feedbackSurvey, assessment.deck_id);
+    mockOption("perspective", "firstPerson");
+
+    component = await ComponentHandler.setup(Component);
+
+    expect(component.tree).toMatchSnapshot();
+  });
+
+  it("renders empty when perspective is not firstPerson", async() => {
+    mockAssessment(assessment, {id: assessment.id});
+    mockUserCompletedFeedback(assessment.id);
+    mockFeedbackSurvey(feedbackSurvey, assessment.deck_id);
+    mockOption("perspective", "thirdPerson");
 
     component = await ComponentHandler.setup(Component);
 

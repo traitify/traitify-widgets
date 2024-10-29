a7804e706ec45f5a87b25c7da7148de4949d0339
Tomify <tprats108@gmail.com>
2024-10-29 16:50:36 -0400

    Added translations

--- src/lib/hooks/use-i18n.js
+++ src/lib/hooks/use-i18n.js
@@ -1,6 +1,21 @@
+import {useEffect, useReducer} from "react";
 import useLoadedValue from "lib/hooks/use-loaded-value";
-import {i18nState} from "lib/recoil";
+import {i18nState, translationsQuery} from "lib/recoil";
 
 export default function useI18n() {
-  return useLoadedValue(i18nState);
+  const i18n = useLoadedValue(i18nState);
+  const translations = useLoadedValue(translationsQuery);
+  const [, forceUpdate] = useReducer((x) => x + 1, 0);
+
+  useEffect(() => {
+    if(!translations) { return; }
+
+    Object.keys(translations).forEach((locale) => {
+      i18n.addTranslations(locale.toLowerCase(), {data: translations[locale.toLowerCase()]});
+    });
+
+    forceUpdate();
+  }, [translations]);
+
+  return i18n;
 }

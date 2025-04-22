f58823ac80d6f6c4132a9a6a237a77990a6806d2
Tomify <tprats108@gmail.com>
2025-04-22 17:42:53 -0400

    Updated version, test, and audit

--- test/components/survey/index.test.js
+++ test/components/survey/index.test.js
@@ -1,10 +1,11 @@
 import Component from "components/survey";
 import ComponentHandler from "support/component-handler";
-import {mockAssessment, mockCognitiveAssessment} from "support/container/http";
+import {mockAssessment, mockCognitiveAssessment, mockExternalAssessment} from "support/container/http";
 import {mockOption} from "support/container/options";
 import useContainer from "support/hooks/use-container";
 import cognitive from "support/json/assessment/cognitive.json";
 import dimensionBased from "support/json/assessment/dimension-based.json";
+import external from "support/json/assessment/external.json";
 
 jest.mock("components/survey/cognitive", () => (() => <div className="mock">Cognitive</div>));
 jest.mock("components/survey/personality", () => (() => <div className="mock">Personality</div>));
@@ -17,6 +18,14 @@ describe("Survey", () => {
   it("renders cognitive", async() => {
     mockCognitiveAssessment(cognitive);
     mockOption("surveyType", "cognitive");
+    component = await ComponentHandler.setup(Component);
+
+    expect(component.tree).toMatchSnapshot();
+  });
+
+  it("renders external", async() => {
+    mockExternalAssessment(external);
+    mockOption("surveyType", "external");
     component = await ComponentHandler.setup(Component);
 
     expect(component.tree).toMatchSnapshot();

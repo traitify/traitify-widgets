80c930fa1077364aee482450c9493ab59f29f1e4
Duy Huynh <duy.huynh@paradox.ai>
2025-08-13 14:31:22 +0700

    remove header and actions

--- src/components/results/generic/index.js
+++ src/components/results/generic/index.js
@@ -3,15 +3,12 @@ import useGraphql from "lib/hooks/use-graphql";
 import useHttp from "lib/hooks/use-http";
 import useResults from "lib/hooks/use-results";
 import Breakdown from "./breakdown";
-import Header from "./header";
-import ResultActions from "./result_actions";
 import Score from "./score";
 import style from "./style.scss";
 
 export default function Generic() {
   const [result, setResult] = useState(null);
   const assessment = useResults({surveyType: "generic"});
-  const profile = result ? result.profile : {};
   const assessmentResult = result ? result.assessment : {};
 
   const graphQL = useGraphql();
@@ -35,9 +32,7 @@ export default function Generic() {
     <div>
       {result && (
         <div className={style.container}>
-          <Header profile={profile} assessment={assessmentResult} />
           <div className={style.contentBody}>
-            <ResultActions />
             <Score assessmentResult={assessmentResult} />
             <Breakdown assessmentResult={assessmentResult} />
           </div>
